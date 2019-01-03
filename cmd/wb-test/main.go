9b49a95d275a5247def8687b8db81fa6a24d83c8
klim0v <klim0v-sergey@yandex.ru>
2019-01-03 16:37:09 +0300

    Done

---
+++ cmd/wb-test/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+)
+
+type Result struct {
+	Url   string
+	Count int
+}
+
+func main() {
+	var wg sync.WaitGroup
+	out := make(chan Result)
+
+	limit := make(chan interface{}, 5)
+	wg.Add(1)
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		wg.Add(1)
+		go sendToPipeline(scanner.Text(), out, limit, &wg)
+	}
+	wg.Done()
+	for res := range out {
+		fmt.Println(fmt.Sprintf("Count for %s: %d", res.Url, res.Count))
+	}
+}
+
+func sendToPipeline(line string, out chan<- Result, limit chan interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer func() {
+		<-limit
+	}()
+	limit <- struct{}{}
+	out <- countQuantity(line)
+}
+
+func countQuantity(url string) (res Result) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	res.Url = url
+	res.Count = strings.Count(string(bytes), "Go")
+	return
+}
